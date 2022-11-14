adbe0a616a3aecc6594b394e7f7ae4d8574707dd
Dhruv Agrawal <dhruvagrawal1055@gmail.com>
2022-11-15 00:29:32 +0530

    first commit

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TimelineComponent } from './timeline/timeline.component';
+const routes: Routes = [
+  {
+    path:'home',
+    component:HomeComponent
+  },
+  {
+    path:'header',
+    component:HeaderComponent
+  },
+  {
+    path:'about',
+    component:AboutComponent
+  },
+  {
+    path:'skills',
+    component:SkillsComponent
+  },
+  {
+    path:'timeline',
+    component:TimelineComponent
+  },
+  {
+    path:'projects',
+    component:ProjectsComponent
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/app/app.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HomeComponent } from './home/home.component';
+import { HeaderComponent } from './header/header.component';
+import{NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { ProjectsComponent } from './projects/projects.component';
+import { AboutComponent } from './about/about.component';
+import { SkillsComponent } from './skills/skills.component';
+import { TimelineComponent } from './timeline/timeline.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    ProjectsComponent,
+    AboutComponent,
+    SkillsComponent,
+    TimelineComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
